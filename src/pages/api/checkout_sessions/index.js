ebe7f6a9140949ff98ba083fc1bef0061737df8c
Marthin <martin.pakpahan@edgeprop.sg>
2023-03-13 17:44:08 +0700

    update

--- src/pages/api/checkout_sessions/index.js
+++ src/pages/api/checkout_sessions/index.js
@@ -22,6 +22,7 @@ export default async function handler(req, res) {
                     credits: 10
                 }),
               });
+            console.log(updateUserResponse);
 
             res.status(200).json(session);
         } catch(err) {
